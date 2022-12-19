a15903e30696ab6c30c5d120b9f231e4b73605dc
ROTISSERIE CHICKEN <98714749+rotisseriechicken@users.noreply.github.com>
2022-12-18 23:31:54 -0600

    selfclient change

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-//  Initializing socket architecture
+//  Initializing socket server architecture
 const options = { cors: { origin: "*" }};
 const io = require("socket.io")(options);
 const PORT = process.env.PORT || 3000;
 
+//  Initializing socket client
+var client_io = require("socket.io-client");
+var client_socket = client_io.connect('https://story-server.onrender.com/', {reconnect: true});
+
 //  Initializing spinner
 const schedule = require('node-schedule');
-const request = require('request');
-
-const job = schedule.scheduleJob('*/4 * * * *', function(){ // Every 4 minutes (agressive downspin...)
-    console.log('🔃 Spinning...');
-    request('https://story-server.onrender.com/', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log('🔃 Spin spunned™');
-      }
-    });
-});
 
 //  Server Variables
 var UserList = []; // List of connected users, and their user objects
@@ -33,7 +27,20 @@ io.on("connect", socket => {
         io.emit('+', word);
     });
 
+    socket.on('p', (content) => {
+      console.log('🔃 Spun™');
+    });
 });
 
+//  Server intiialization
 io.listen(PORT); // Listen on server-designated port
 console.log('Server started on port ' + PORT);
+
+//  Self-client initiailization (spinner)
+client_socket.on('connect', function (socket) {
+    console.log('Connected to self socket');
+    const job = schedule.scheduleJob('*/2 * * * *', function(){ // Every 2 minutes (agressive downspin...)
+        console.log('🔃 Spinning...');
+        client_socket.emit('p', '.');
+    });
+});
