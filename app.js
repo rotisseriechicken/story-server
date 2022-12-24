7155446730cfa1c06b91946c2da61a1ea6c2427b
ROTISSERIE CHICKEN <98714749+rotisseriechicken@users.noreply.github.com>
2022-12-24 02:46:39 -0600

    v2

--- app.js
+++ app.js
@@ -33,13 +33,19 @@ const schedule = require('node-schedule');
  *            CONTENT:  [{The word object containing the w packet's word as a string}, waitlist time]
  *
  *      f  -  Server stating the story is finished and moving onto the next one. (EMITTED TO ALL!)
- *            CONTENT:  The number of the new story as an integer.
+ *            CONTENT:  [The integer of the new story, The Date.now() UTC time of the next game start]
  *
  *      v  -  Update vote score of word. (EMITTED TO ALL!)
  *            CONTENT:  [The index of the word in the story array, & its new score total], both as ints.
  *
- *      q  -  Keep-alive ping. Response is expected within 15,000 MS, or the client has disconnected.
- *            CONTENT:  "."
+ *      A  -  Userlist update (and all in-progress words).
+ *            CONTENT:  [MODE as an integer, [[UUID, prognostication], [UUID, prognostication etc]]]
+ *
+ *      J  -  User joined.
+ *            CONTENT:  [UUID]
+ *
+ *      L  -  User left.
+ *            CONTENT:  [UUID, Reason]
  *
  *      r  -  Message rejected.
  *            CONTENT:  [Rejection reason as an integer, Words until submittable (incl. 0) as an integer]
@@ -63,9 +69,9 @@ const schedule = require('node-schedule');
  *            CLIENT:    Index of the word in the story array as an integer.
  *            RESPONSE:  v packet containing [the word index, and new vote total] as integers.
  *
- *      A  -  Keep-alive ping. Response to server's q packet.
- *            CLIENT:    "!"
- *            RESPONSE:  <none>
+ *      *  -  In-progress text
+ *            CLIENT:    "spaghetti", or any non-slur in-progress string < 35 chars.
+ *            RESPONSE:  A packet, a variable number of times a second, with all user deltas.
  *
  *      p  -  Client lite ping. Intended as self-spin.
  *            CLIENT:    <any data>
@@ -74,37 +80,28 @@ const schedule = require('node-schedule');
 
 // #######################################################################################
 //  Server Variables
-var VERSION = 1; // Server's version; Used to validate major changes with the client
-var UserList = []; // List of connected users, and their user objects; Used to track cooldowns.
-var UserObject = {}; // Object of pointers to connected users
+var VERSION = 2; // Server's version; Used to validate major changes with the client
+var UserObject = {}; // Object of arrays: 
+// socket.id: [socket object pointer, UUID, prognostication string]
 
-var ActiveUserList = []; // List of all user IDs that have recently submitted a word
+var Prognostication_Delta = []; // list of UUIDs and their strings which need updating next prog cycle
+
 var WaitList = []; // List of users that are required to wait before submitting further entries.
 
 var STORY = []; // The story data so far
 var WHICH_STORY = 0; // The number of story currently in progress
+var STORY_ACTIVATE_TIME = Date.now(); // The time at which the next story will begin.
+
+var CUTSCENE_TIME = 0; // 15000; // Server-enforced time between games (cutscene duration!)
 
 var FLAG_SPUN_ONCE = false; // If true, the spinner has begun
+var ITERATIVE_UUID = 0; // this number will be new users' iterating UUID
 
 
 
 // #######################################################################################
 //  Server functions
-function checkUsers(){
-  for(var USER of UserList){
-    UserObject[USER].timeout(15000).emit("q", (err) => {
-      if (err) {
-        // client considered disconnected
-        var DISCONNECTED_USER = USER;
-        delete UserObject[USER];
-        UserList.splice(UserList.findIndex(elem => elem === USER), 1);
-        console.log('X<-- User ' + DISCONNECTED_USER + ' disconnected (' + UserList.length + ' connected)');
-      }
-    });
-  }
-}
-
-function checkActiveUsers(){
+function updateActiveUsers(){
 
 }
 
@@ -117,10 +114,32 @@ function decrementWaitlist(){
   }
 }
 
+function getFullUserdata(){
+  var CompiledUserdata = [];
+  for(var USER in UserObject){
+      CompiledUserdata.push([UserObject[USER][1],UserObject[USER][2]]);
+  }
+  return CompiledUserdata;
+}
+
+function updateUserdata(){
+
+}
+
+function currentlyOnline(){
+  return Object.keys(UserObject).length;
+}
+
+function updatePrognostications(socketpointer){
+  socketpointer.broadcast.emit('A', [1, Prognostication_Delta]); // mode 1 for update
+  Prognostication_Delta = [];
+}
+
+
 
 // #######################################################################################
 //  Client function parity
-function validateWord(word){ // Input word, assumed at this point to be a string
+function VALIDATOR(word){ // Input word, assumed at this point to be a string
   if(word != ''){
     return true; // word has substance
   } else {
@@ -142,10 +161,32 @@ function HTMLcleanString(UNSAFE_STRING){
 io.on("connect", socket => {
 
     //  On new client connecting to server
-    UserList.push(socket.id); // Add to list of connected users
-    UserObject[socket.id] = socket; // Add pointer to object referenced by array
-    console.log('O--> User ' + socket.id + ' connected (' + UserList.length + ' connected)');
-    socket.emit('c', [WHICH_STORY, STORY, VERSION]);
+    UserObject[socket.id] = [socket, ITERATIVE_UUID, '']; // Add pointer to object referenced by array
+    var GAME_MODE_NOW = 0; // In an active game
+    if(Date.now() < STORY_ACTIVATE_TIME){
+      GAME_MODE_NOW = 1; // Cutscene
+    }
+    socket.emit('c', [WHICH_STORY, STORY, VERSION, ITERATIVE_UUID, GAME_MODE_NOW, [0,getFullUserdata()]]);
+    socket.broadcast.emit('J', ITERATIVE_UUID); // emit to all but joiner that a new client has joined
+    console.log('O--> User ' + socket.id + ' (UUID '+ITERATIVE_UUID+') connected (' + currentlyOnline() + ' connected)');
+    ITERATIVE_UUID++; // iterate UUID list
+
+    //  On client disconnecting from server for any reason
+    socket.on("disconnect", (reason) => {
+      var DISCONNECTED_USER = [socket.id, UserObject[socket.id][1]];
+      socket.broadcast.emit('L', DISCONNECTED_USER[1]); // emit to all but joiner that this client left
+      delete UserObject[socket.id];
+      // UserList.splice(UserList.findIndex(elem => elem === USER), 1);
+      console.log('X<-- User ' + DISCONNECTED_USER[0] + ' (UUID '+DISCONNECTED_USER[1]+') disconnected (' + currentlyOnline() + ' connected)');
+    });
+
+    //  On new prognostication update
+    socket.on('*', (word) => {
+      var LIMITED_WORD = HTMLcleanString(word.substring(0,35));
+      UserObject[socket.id][2] = LIMITED_WORD;
+      Prognostication_Delta.push([UserObject[socket.id][1], UserObject[socket.id][2]]);
+      updatePrognostications(socket);
+    }
 
     //  On new word from a submitter
     socket.on('w', (word) => { //  Update story--and emit new entry--if this submission passes inspection
@@ -163,67 +204,74 @@ io.on("connect", socket => {
         var WaitListInd = WaitList.findIndex(elem => elem[0] === socket.id);
         if(WaitListInd == -1){ // If user is not on the waitlist,
 
-          var VALID = validateWord(CLEANWORD);
-          if(VALID){ // and the word they submitted is valid, then submit the word
+          if(STORY_ACTIVATE_TIME <= Date.now()){
 
-            var WORD_OBJECT = {
-              word: CLEANWORD,
-              by: socket.id,
-              at: Date.now(),
-              votes: 0
-            }
+            var VALID = VALIDATOR(CLEANWORD);
+            if(VALID){ // and the word they submitted is valid, then submit the word
 
-            //  Decrement the waitlist for users on it
-            decrementWaitlist(); 
-            var WaitlistedFor = 0; // 0 words waitlist by default
+              var WORD_OBJECT = {
+                word: CLEANWORD,
+                by: UserObject[socket.id][1], // UUID
+                at: Date.now(),
+                votes: 0
+              }
 
-            /*
+              //  Decrement the waitlist for users on it
+              decrementWaitlist(); 
+              var WaitlistedFor = 0; // 0 words waitlist by default
 
-            //  If userlist is over a certain number of people, engage the waitlist respectively
-            if(UserList.length >= 2){ // <----- MAKE THIS NUMBER 6 AFTER TESTING #################!!!!!
-              if(UserList.length >= 20){
-                if(UserList.length >= 100){
-                  if(UserList.length >= 500){
-                    if(UserList.length >= 2500){
-                      WaitList.push([socket.id, 25]); // 2500+ users ----- 25 word waitlist
-                      WaitlistedFor = 25;
+              /*
+
+              //  If userlist is over a certain number of people, engage the waitlist respectively
+              if(currentlyOnline() >= 2){ // <----- MAKE THIS NUMBER 6 AFTER TESTING #################!!!!!
+                if(currentlyOnline() >= 20){
+                  if(currentlyOnline() >= 100){
+                    if(currentlyOnline() >= 500){
+                      if(currentlyOnline() >= 2500){
+                        WaitList.push([socket.id, 25]); // 2500+ users ----- 25 word waitlist
+                        WaitlistedFor = 25;
+                      } else {
+                        WaitList.push([socket.id, 10]); // 500-2499 users -- 10 word waitlist
+                        WaitlistedFor = 10;
+                      }
                     } else {
-                      WaitList.push([socket.id, 10]); // 500-2499 users -- 10 word waitlist
-                      WaitlistedFor = 10;
+                      WaitList.push([socket.id, 4]); // 100-499 users ----- 4 word waitlist
+                      WaitlistedFor = 4;
                     }
                   } else {
-                    WaitList.push([socket.id, 4]); // 100-499 users ----- 4 word waitlist
-                    WaitlistedFor = 4;
+                    WaitList.push([socket.id, 2]); // 20-99 users ---------- 2 word waitlist
+                    WaitlistedFor = 2;
                   }
                 } else {
-                  WaitList.push([socket.id, 2]); // 20-99 users ---------- 2 word waitlist
-                  WaitlistedFor = 2;
+                  WaitList.push([socket.id, 1]); // 6-19 users ------------- 1 word waitlist
+                  WaitlistedFor = 1;
                 }
-              } else {
-                WaitList.push([socket.id, 1]); // 6-19 users ------------- 1 word waitlist
-                WaitlistedFor = 1;
               }
+
+              */
+
+              //  Add to story
+              STORY.push(WORD_OBJECT);
+              socket.broadcast.emit('+', [WORD_OBJECT]);
+              socket.emit('+', [WORD_OBJECT, WaitlistedFor]);
+              console.log('UUID ' + UserObject[socket.id][1] + ':  ' + CLEANWORD);
+
+              UserObject[socket.id][2] = '';
+
+              //  if story reaches 100 words, emit the Finished message
+              if(STORY.length == 100){
+                WHICH_STORY++;
+                //  Insert line which calls function that submits the story to RCW endpoint here!
+                STORY = [];
+                console.log('>>> Story completed, beginning story ' + WHICH_STORY);
+                io.emit('f', [WHICH_STORY, (Date.now() + CUTSCENE_TIME)]);
+              }
+
+            } else { // Invalid word
+              socket.emit('r', [0, 0]);
             }
-
-            */
-
-            //  Add to story
-            STORY.push(WORD_OBJECT);
-            socket.broadcast.emit('+', [WORD_OBJECT]);
-            socket.emit('+', [WORD_OBJECT, WaitlistedFor]);
-            console.log(CLEANWORD);
-
-            //  if story reaches 100 words, emit the Finished message
-            if(STORY.length == 100){
-              WHICH_STORY++;
-              //  Insert line which calls function that submits the story to RCW endpoint here!
-              STORY = [];
-              console.log('>>> Story completed, beginning story ' + WHICH_STORY);
-              io.emit('f', WHICH_STORY);
-            }
-
-          } else { // Invalid word
-            socket.emit('r', [0, 0]);
+          } else { // The next story has not yet begun
+          socket.emit('r', [2, 0]);
           }
         } else { // User waitlisted
           socket.emit('r', [1, WaitList[WaitListInd][1]]);
@@ -241,9 +289,8 @@ console.log('Server started on port ' + PORT);
 
 //  Ping initialization
 const deadusers = schedule.scheduleJob('59 * * * * *', function(){ // Every minute (agressive downspin...)
-    console.log('Updating user count...');
-    checkUsers();
-    checkActiveUsers();
+    console.log('Updating user activity...');
+    updateActiveUsers();
 });
 
 //  Self-client initiailization (spinner)
